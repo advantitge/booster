29832b85c0eaf89af622ae4affa7ec6977162730
hendrikdepauw <hendrik@advantitge.com>
2021-08-06 15:24:02 +0200

    feat: createdAt as string

--- packages/framework-provider-micro/src/library/events-searcher-adapter.ts
+++ packages/framework-provider-micro/src/library/events-searcher-adapter.ts
@@ -9,13 +9,15 @@ import {
 import { Filter } from 'mongodb'
 import { getCollection } from '../services/db'
 
+type DatabaseEventEnvelope = Omit<EventEnvelope, 'createdAt'> & { createdAt: Date }
+
 export async function searchEvents(
   config: BoosterConfig,
   logger: Logger,
   filters: EventFilter
 ): Promise<Array<EventSearchResponse>> {
   logger.debug('Initiating an events search. Filters: ', filters)
-  const query: Filter<Omit<EventEnvelope, 'createdAt'> & { createdAt: Date }> = {
+  const query: Filter<DatabaseEventEnvelope> = {
     kind: { $eq: 'event' },
     createdAt: {
       $gte: filters.from ? new Date(filters.from) : new Date(0),
@@ -33,10 +35,11 @@ export async function searchEvents(
     throw new Error('Invalid search event query. It is neither an search by "entity" nor a search by "type"')
   }
   const collection = await getCollection(config.resourceNames.eventsStore)
-  const eventEnvelopes = await collection.find(query).sort({ createdAt: -1 }).toArray<EventEnvelope>()
+  const eventEnvelopes = await collection.find(query).sort({ createdAt: -1 }).toArray<DatabaseEventEnvelope>()
   logger.debug('Events search result: ', eventEnvelopes)
   return eventEnvelopes.map((e) => ({
     ...e,
+    createdAt: e.createdAt.toJSON(),
     type: e.typeName,
     entity: e.entityTypeName,
     value: e.value as EventInterface,
